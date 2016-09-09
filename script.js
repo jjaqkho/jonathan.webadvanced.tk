043a540176c955172ce9424802ead5a2646a8db4
Jonathan Kho <khoj122@newschool.edu>
2016-09-09 00:01:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('hello world');
+// wait for document to load
+$(document).ready(function() {
+
+  $('body').css('display', 'none');
+  $('body').fadeIn(20);
+  $('.invert').click(
+    function() {
+      event.preventDefault();
+      newLocation = this.href;
+      $('body').fadeOut(200, newpage);
+    });
+
+  function newpage() {
+    window.location = newLocation;
+  }
+
+});
